2d43f2a735f24b3e14853dc0d513404bccb888fb
Emmanueladimchinaobi <deborahamybliss@gmail.com>
2026-07-08 00:34:49 +0100

    first commit

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const menu = document.getElementById("menu");
 
 
 btn.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
+  menu.classList.toggle("-translate-x-full");
+  menu.classList.toggle("-translate-x-0");
+  menu.classList.toggle("opacity-0");
+
 });
 
 
